005414969593d0c6876254d6acdf895d37cf1bfe
lameturkey <koenk1@gmail.com>
2018-11-12 22:16:20 +0100

    added json functionality

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -1,11 +1,11 @@
 import csv
 import pandas
 import matplotlib.pyplot as plt
+import json
 
-titles = ["Country", "Region", "Pop. Density (per sq. mi.)", "Infant mortality (per 1000 births)" , "GDP ($ per capita) dollars"]
 def read():
     with open('input.csv') as csvfile:
-
+        titles = ["Country", "Region", "Pop. Density (per sq. mi.)", "Infant mortality (per 1000 births)" , "GDP ($ per capita) dollars"]
         # read the csv
         dictreader = csv.DictReader(csvfile)
         dictionarylist = []
@@ -30,21 +30,32 @@ def clean(dicts):
                 dict[key] = float(dict[key].replace(',','.'))
     return dicts
 
+def save_json(pandadict):
+    with open('output.json', 'w+') as json_file:
+        json.dump(pandadict.to_dict(orient='index'), json_file,  indent=4)
+
 if __name__ == '__main__':
+    # read the data
     dicts = read()
+
+    # process the data
     dicts = clean(dicts)
-    pandadict = pandas.DataFrame(dicts)
+
+    # convert to pandaframe
+    pandadict = pandas.DataFrame(dicts, [dict["Country"] for dict in dicts])
+    pandadict = pandadict.drop("Country", axis=1)
+        pandadict = pandadict[abs(pandadict.GDP - pandadict.GDP.mean()) < 2 * pandadict.GDP.std()]
     # http://www.datasciencemadesimple.com/mean-function-python-pandas-dataframe-row-column-wise-mean/
     print(pandadict.GDP.mean())
     print(pandadict.GDP.median())
     print(pandadict.GDP.std())
+
+    # removes all values more than 3 stdev away from the mean
     #https://stackoverflow.com/questions/23199796/detect-and-exclude-outliers-in-pandas-dataframe
-    pandadict[abs(pandadict.GDP - pandadict.GDP.mean()) < 2 * pandadict.GDP.std()].to_string()
-    print(pandadict.to_string())
-    plt.figure()
     plt.grid(True)
-    histogram = pandadict.hist(column='GDP')
-    print(histogram)
+    histogram = pandadict.GDP.plot.hist().plot()
+    # plt.show()
     #https://stackoverflow.com/questions/34347145/pandas-plot-doesnt-show
     boxplot = pandadict.boxplot(column='Infant').plot()
-    plt.show()
+    # plt.show()
+    save_json(pandadict)

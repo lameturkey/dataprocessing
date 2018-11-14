ab437fe56a8e4f3db2deb81a6fda4c40ec72f4b4
lameturkey <koenk1@gmail.com>
2018-11-14 13:19:20 +0100

    SOLVED EVERYTHING SUCH WOW

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# Name: Koen van der Kamp
+# Student number: 12466573
+# Graph should be fullscreen for optimal readability
+
 import csv
 import pandas
 import matplotlib.pyplot as plt
@@ -5,23 +10,31 @@ import json
 
 def read():
     with open('input.csv') as csvfile:
+
+        # define the wanted titles
         titles = ["Country", "Region", "Pop. Density (per sq. mi.)", "Infant mortality (per 1000 births)" , "GDP ($ per capita) dollars"]
+
         # read the csv
         dictreader = csv.DictReader(csvfile)
         dictionarylist = []
 
-        # save the csv to dict
+        # save the csv to a list of dicts (only wanted titles)
         for dict in dictreader:
             newdict = {}
             for key in dict:
                 if key in titles:
                     newdict[key.split(' ')[0].strip('.')] = dict[key].strip()
             dictionarylist.append(newdict)
+
         # return the dict
         return dictionarylist
 
 def clean(dicts):
+
+    # remove the dicts that contain faulty values
     dicts = [dict for dict in dicts if not ('' in dict.values()  or 'unknown' in dict.values())]
+
+    # clean the values to int or float
     for dict in dicts:
         for key in dict:
             if key == "GDP":
@@ -31,10 +44,13 @@ def clean(dicts):
     return dicts
 
 def save_json(pandadict):
+
+    # save to json
     with open('output.json', 'w+') as json_file:
         json.dump(pandadict.to_dict(orient='index'), json_file,  indent=4)
 
 if __name__ == '__main__':
+
     # read the data
     dicts = read()
 
@@ -51,10 +67,10 @@ if __name__ == '__main__':
 
     # http://www.datasciencemadesimple.com/mean-function-python-pandas-dataframe-row-column-wise-mean/
     # print the central tendency
-    print("GDP per capita")
+    print("GDP per capita central tendency")
     print(f"Mean: {int(pandadict.GDP.mean())}")
     print(f"Median: {int(pandadict.GDP.median())}")
-    print(f"Standard deviation  {int(pandadict.GDP.std())}")
+    print(f"Standard deviation  {int(pandadict.GDP.std())} \n")
 
     # print 5 number summary
     print("The 5 number summary of Infant mortality (per 1000 births) per country")
@@ -65,14 +81,21 @@ if __name__ == '__main__':
     print(f"Maximum: {pandadict.Infant.max()}")
 
     #produce a histogram for the GDP
-    plt.title("GDP per capita with different countries histogram")
-    histogram = pandadict.GDP.hist(bins=20).plot()
+    plt.title("GDP per capita of different countries histogram")
+    histogram = pandadict.GDP.hist(bins=20).set_xticks([x for x in range(0, 60000, 5000)])
+    plt.xlabel("GDP per capita")
+    plt.ylabel("bin count")
+    # histogram.plot()
     plt.show()
-    plt.title("Infant mortality rates boxplot")
 
     # produce a boxplot
+    plt.title("Infant mortality rates of different countries boxplot")
+    plt.ylabel("Infant mortality (per 1000 births)")
+
     boxplot = pandadict.boxplot(column='Infant', grid=False)
     boxplot.axes.set_xticks([], minor=False)
     boxplot.plot()
     plt.show()
+
+    # save to json
     save_json(pandadict)

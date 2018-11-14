33551651bfd1dd5dfdd8d17ec62188177510a50d
lameturkey <koenk1@gmail.com>
2018-11-14 10:29:45 +0100

    LOL

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -44,18 +44,35 @@ if __name__ == '__main__':
     # convert to pandaframe
     pandadict = pandas.DataFrame(dicts, [dict["Country"] for dict in dicts])
     pandadict = pandadict.drop("Country", axis=1)
-        pandadict = pandadict[abs(pandadict.GDP - pandadict.GDP.mean()) < 2 * pandadict.GDP.std()]
-    # http://www.datasciencemadesimple.com/mean-function-python-pandas-dataframe-row-column-wise-mean/
-    print(pandadict.GDP.mean())
-    print(pandadict.GDP.median())
-    print(pandadict.GDP.std())
 
-    # removes all values more than 3 stdev away from the mean
-    #https://stackoverflow.com/questions/23199796/detect-and-exclude-outliers-in-pandas-dataframe
-    plt.grid(True)
-    histogram = pandadict.GDP.plot.hist().plot()
-    # plt.show()
-    #https://stackoverflow.com/questions/34347145/pandas-plot-doesnt-show
-    boxplot = pandadict.boxplot(column='Infant').plot()
-    # plt.show()
+    #remove all values more than 2 stdev away from the mean
+    # https://stackoverflow.com/questions/23199796/detect-and-exclude-outliers-in-pandas-dataframe
+    pandadict = pandadict[abs(pandadict.GDP - pandadict.GDP.mean()) < 2 * pandadict.GDP.std()]
+
+    # http://www.datasciencemadesimple.com/mean-function-python-pandas-dataframe-row-column-wise-mean/
+    # print the central tendency
+    print("GDP per capita")
+    print(f"Mean: {int(pandadict.GDP.mean())}")
+    print(f"Median: {int(pandadict.GDP.median())}")
+    print(f"Standard deviation  {int(pandadict.GDP.std())}")
+
+    # print 5 number summary
+    print("The 5 number summary of Infant mortality (per 1000 births) per country")
+    print(f"Minimum: {pandadict.Infant.min()}")
+    print(f"First Quartile: {pandadict.Infant.quantile(0.25)}")
+    print(f"Median: {pandadict.Infant.median()}")
+    print(f"Third Quartile: {pandadict.Infant.quantile(0.75)}")
+    print(f"Maximum: {pandadict.Infant.max()}")
+
+    #produce a histogram for the GDP
+    plt.title("GDP per capita with different countries histogram")
+    histogram = pandadict.GDP.hist(bins=20).plot()
+    plt.show()
+    plt.title("Infant mortality rates boxplot")
+
+    # produce a boxplot
+    boxplot = pandadict.boxplot(column='Infant', grid=False)
+    boxplot.axes.set_xticks([], minor=False)
+    boxplot.plot()
+    plt.show()
     save_json(pandadict)
